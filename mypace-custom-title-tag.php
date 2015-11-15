afc00a76a794a452eec94d18d465f2c5cc99bc87
ozonenotes Kei Nomura <kei@ozonenotes.jp>
2015-11-15 14:09:08 +0900

    Change functions' name, define a class.

--- mypace-custom-title-tag.php
+++ mypace-custom-title-tag.php
@@ -10,103 +10,104 @@ Text Domain: mypace-custom-title-tag
 Domain Path: /languages
 */
 
-function MCTT_plugins_loaded() {
-	load_plugin_textdomain( 'mypace-custom-title-tag', false, basename( dirname( __FILE__ ) ) . '/languages' );
-}
+if ( !class_exists( 'Mypace_Custom_Title_Tag' ) ){
 
-if ( !function_exists( 'mypace_add_meta_box' ) ){
-	//make a meta box
-	function mypace_add_meta_box(){
-		$post_types = wp_list_filter(
-				get_post_types(array('public' => true)),
-				array('attachment'),
-				'NOT'
-		);
-		foreach ( $post_types as $post_type ){
-			add_meta_box(
-				'mypace_sectionid',
-				esc_html__( 'Title Tag', 'mypace-custom-title-tag' ),
-				'mypace_title_meta_box',
-				$post_type,
-				'advanced'
+	class Mypace_Custom_Title_Tag{
+
+		public function __construct() {
+			//Actions and Filters	
+			add_action( 'admin_menu', array( $this, 'add_meta_box' ) );
+			add_action( 'save_post',  array( $this, 'save_titledata' ) );
+			add_filter( 'wp_title',   array( $this, 'custom_title' ) );
+			load_plugin_textdomain( 'mypace-custom-title-tag', false, basename( dirname( __FILE__ ) ) . '/languages' );
+		}
+
+		//make a meta box
+		public function add_meta_box(){
+			$post_types = wp_list_filter(
+					get_post_types(array('public' => true)),
+					array('attachment'),
+					'NOT'
 			);
+			foreach ( $post_types as $post_type ){
+				add_meta_box(
+					'mypace-title-tag',
+					esc_html__( 'Title Tag', 'mypace-custom-title-tag' ),
+					array( $this, 'title_meta_box' ),
+					$post_type,
+					'advanced'
+				);
+			}
 		}
+
+		public function title_meta_box(){
+			//input form
+			wp_nonce_field( plugin_basename(__FILE__), 'mypace_noncename' );
+			$field_name = 'mypace_title_tag';
+			$field_value = get_post_meta( get_the_ID(), $field_name, true );
+
+			printf(
+				'<label for="%s">%s</label><br />',
+				esc_attr($field_name),
+				esc_html__( "Enter title-tag text.", 'mypace-custom-title-tag' )
+			);
+			printf(
+				'<input type="text" name="%s" value="%s" size="40" />',
+				esc_attr($field_name),
+				esc_attr($field_value)
+			);
 	}
-}
 
-if ( !function_exists( 'mypace_title_meta_box' ) ){
-	function mypace_title_meta_box(){
-		//input form
-		wp_nonce_field( plugin_basename(__FILE__), 'mypace_noncename' );
-		$field_name = 'mypace_title_tag';
-		$field_value = get_post_meta( get_the_ID(), $field_name, true );
+		public function save_titledata($post_id){
 
-		printf(
-			'<label for="%s">%s</label><br />',
-			esc_attr($field_name),
-			esc_html__( "Enter title-tag text.", 'mypace-custom-title-tag' )
-		);
-		printf(
-			'<input type="text" name="%s" value="%s" size="40" />',
-			esc_attr($field_name),
-			esc_attr($field_value)
-		);
-	}
-}
-
-if ( !function_exists( 'mypace_save_titledata' ) ){
-	function mypace_save_titledata($post_id){
-
-		//permission check and save data
-		if ( !isset($_POST['mypace_noncename']) ){
-			return $post_id;
-		}
-		if ( !wp_verify_nonce( $_POST['mypace_noncename'], plugin_basename(__FILE__) ) ){
-			return $post_id;
-		}
-		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ){
-			return $post_id;
-		}
-
-		$post_type = isset( $_POST['post_type'] ) ? $_POST['post_type'] : '';
-		$post_types = wp_list_filter(
-				get_post_types(array('public' => true)),
-				array('attachment'),
-				'NOT'
-		);
-		if ( in_array($post_type, $post_types) ){
-			if ( !current_user_can( 'edit_' . $post_type, $post_id ) ){
+			//permission check and save data
+			if ( !isset($_POST['mypace_noncename']) ){
 				return $post_id;
 			}
-		} else {
-			return $post_id;
-		}
-
-		$mydata = isset($_POST['mypace_title_tag']) ? $_POST['mypace_title_tag'] : '';
-		if ( !empty($mydata) ){
-			update_post_meta( $post_id, 'mypace_title_tag', $mydata );
-		} else {
-			delete_post_meta( $post_id, 'mypace_title_tag' );
-		}
-		return $mydata;
-	}
-}
-
-if ( !function_exists( 'mypace_custom_title' ) ){
-	//output title tag
-	function mypace_custom_title( $title ){
-		if( is_singular() ){
-			$post_id = get_the_ID();
-			$my_title = get_post_meta( $post_id, 'mypace_title_tag', true );
-			if( $my_title ){
-				return esc_html( $my_title );
+			if ( !wp_verify_nonce( $_POST['mypace_noncename'], plugin_basename(__FILE__) ) ){
+				return $post_id;
 			}
-		}
-		return $title;
-	}
-}
+			if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ){
+				return $post_id;
+			}
 
-add_action( 'plugins_loaded', 'MCTT_plugins_loaded' );
-add_action( 'admin_menu',     'mypace_add_meta_box' );
-add_action( 'save_post',      'mypace_save_titledata' );
-add_filter( 'wp_title',       'mypace_custom_title' );
+			$post_type = isset( $_POST['post_type'] ) ? $_POST['post_type'] : '';
+			$post_types = wp_list_filter(
+					get_post_types(array('public' => true)),
+					array('attachment'),
+					'NOT'
+			);
+			if ( in_array($post_type, $post_types) ){
+				if ( !current_user_can( 'edit_' . $post_type, $post_id ) ){
+					return $post_id;
+				}
+			} else {
+				return $post_id;
+			}
+
+			$mydata = isset($_POST['mypace_title_tag']) ? $_POST['mypace_title_tag'] : '';
+			if ( !empty($mydata) ){
+				update_post_meta( $post_id, 'mypace_title_tag', $mydata );
+			} else {
+				delete_post_meta( $post_id, 'mypace_title_tag' );
+			}
+			return $mydata;
+		}
+
+		//output title tag
+		public function custom_title( $title ){
+			if( is_singular() ){
+				$post_id = get_the_ID();
+				$my_title = get_post_meta( $post_id, 'mypace_title_tag', true );
+				if( $my_title ){
+					return esc_html( $my_title );
+				}
+			}
+			return $title;
+		}
+
+	}
+
+	new Mypace_Custom_Title_Tag();
+
+}
--- readme.txt
+++ readme.txt
@@ -2,38 +2,56 @@
 Contributors: mypacecreator
 Donate link: http://www.amazon.co.jp/registry/wishlist/33HK9YOKDESUO
 Tags: SEO, title
-Requires at least: 4.0
+Requires at least: 4.1
 Tested up to: 4.3.1
 Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-
 
 == Description ==
 
 This plugin allows you to edit title tag at every singular post(posts, pages, custom post types).
 This is a very simple plugin.
 
-= Contributors =
-
-* [Kei Nomura](http://mypacecreator.net/) ( [mypacecreator](https://profiles.wordpress.org/mypacecreator/) )
 
 == Installation ==
 
 1. Upload `mypace Custom Title Tag` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 
+= Notice =
+
+Your theme must use`add_theme_support( 'title-tag' );` in the functions.php file in order to support title tag.
+No need to add anything to your theme’s header.php file, just remove the legacy wp_title() call or all hard-coded title tags.
+
+* [Related: Title Tag « WordPress Codex](https://codex.wordpress.org/Title_Tag/) 
+
+
 == Frequently Asked Questions ==
 
-Do you have questions or issues with mypace Custom SEO Metatag? Use these support channels appropriately.
+= This plugin output nothing. =
 
-1. [Twitter](https://twitter.com/mypacecreator)
+Your theme must use`add_theme_support( 'title-tag' );` in the functions.php file in order to support title tag.
+
+= Title tags duplicated. =
+
+No need to add anything to your theme’s header.php file, just remove the legacy wp_title() call or all hard-coded title tags.
+
+* [Related: Title Tag « WordPress Codex](https://codex.wordpress.org/Title_Tag/) 
+
+== Screenshots ==
+
+1. Enter text into Title tag field.
+2. Edited title tag.
 
 == Changelog ==
+
+= 1.1 =
+* Change functions' name, define a class.
 
 = 1.0 =
 * Initial release.
 
-= GitHub =
+== GitHub ==
 
 https://github.com/mypacecreator/mypace-custom-title-tag
